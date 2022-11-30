d27836a1bd11292e0026e3ae254378832b048af3
Carlos Jose Fernandes <fernac03@gmail.com>
2022-11-30 14:36:20 -0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -37,6 +37,8 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.helpers.entity import DeviceInfo
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
+from homeassistant.helpers import device_registry as dr
+
 
 from .const import (
     DATA_AD,
